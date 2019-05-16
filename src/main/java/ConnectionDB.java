8a6443aedecc2637a633e64f97c7ff6937522465
pashavik <pash.petrow2011@yandex.ru>
2019-05-16 19:07:06 +0300

    //

--- src/main/java/ConnectionDB.java
+++ src/main/java/ConnectionDB.java
@@ -8,11 +8,32 @@ public class ConnectionDB {
         Connection connection=DriverManager.getConnection("jdbc:h2:tcp://localhost/~/test", "sa", "");
 
        Statement statement= connection.createStatement();
-        ResultSet resultSet=statement.executeQuery("select * from user");
-        while (resultSet.next()){
-            System.out.println(resultSet.getLong(1));
-            System.out.println(resultSet.getString("login"));
+
+
+//        ResultSet resultSet=statement.executeQuery("select * from user");
+//        while (resultSet.next()){
+//            System.out.println(resultSet.getLong(1));
+//            System.out.println(resultSet.getString("login"));
+//        }
+
+//        statement.addBatch("update user set blocked='1' where login='first'");
+//        statement.addBatch("update user set org_id='1' where login='third'");
+//        statement.addBatch("delete from user  where login='second'");
+//        statement.executeBatch();
+
+        connection.setAutoCommit(false);
+        Savepoint savepoint=connection.setSavepoint();
+        try(Statement statement1=connection.createStatement()){
+            statement.execute("update account set balance=balance+100 where id=1");
+            statement.execute("update account set balance=balance-100 where id=2");
+
+            connection.commit();
+        } catch (SQLException e){
+            connection.rollback(savepoint);
+            throw new RuntimeException();
+            
         }
+
 
 
     }
